8fa2abbbe1933bd484dfd0354670fa827238ac53
GOZIECHUKWU CHIMA-DURU <58693993+ByteEngr@users.noreply.github.com>
2024-09-13 17:22:05 +0800

    Add __init__.py

---
+++ src/dsp_scanner/federated_learning/__init__.py
@@ -0,0 +1,11 @@
+"""
+Federated learning modules.
+"""
+
+from dsp_scanner.federated_learning.federated_engine import (
+    FederatedLearningEngine,
+    FederatedModelUpdate,
+    FederatedRound,
+)
+
+__all__ = ["FederatedLearningEngine", "FederatedModelUpdate", "FederatedRound"]
